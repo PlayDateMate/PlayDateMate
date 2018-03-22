10cb22acffd15273cca42aedc0a3632e5afb754e
Tyler <tylerkonesky@hotmail.com>
2018-03-22 12:00:49 -0600

    dashboard updates

--- Views/Dashboard/Dashboard.js
+++ Views/Dashboard/Dashboard.js
@@ -6,7 +6,7 @@ import {StackRouter} from 'react-navigation'
 
 import Friends from '../Friends/Friends'
 import Events from '../Events/Events'
-
+import Profile from '../Profile/Profile'
 
 import{
     StyleSheet, 
@@ -19,6 +19,14 @@ import{
 
 
 export default class Dashboard extends Component{
+    constructor(){
+        super();
+        this.state = {
+            user_name: '',
+            zip_code: '',
+            children: ''
+        }
+    }
     render(){
         return(
             
@@ -45,7 +53,22 @@ export default class Dashboard extends Component{
                 justifyContent: 'center',
                 
             }}>
-            
+            {/* // ***************************** To Do List ******************************** */}
+            <View>
+                {this.state.zip_code ? <Text></Text> :
+                <Text style={{color:'red'}} onPress={()=> this.props.navigation.navigate('Profile')}>Your Zip Code needs to be updated!</Text>
+
+                }
+            </View>
+            <View>
+                {this.state.children ? <Text></Text> :
+                <Text style={{color:'red'}} onPress={()=> this.props.navigation.navigate('Profile')}>Your Children need to be updated!</Text>
+
+                }
+            </View>
+
+            {/* // ***************************** End To Do List ********************************** */}
+
             <View style={{ borderWidth:.5, borderColor:'gray',  borderRadius: 4, width: 250, margin: 50, backgroundColor: 'blue', alignItems:'center', height: 100, justifyContent:'center'}}>
                 <Text style={{  color: 'white', 
                                 
