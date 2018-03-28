5500e572061a43b07a7926e72c0262acfb76a954
Tyler <tylerkonesky@hotmail.com>
2018-03-28 10:59:26 -0600

    working branch

--- Views/Profile/Profile.js
+++ Views/Profile/Profile.js
@@ -35,7 +35,7 @@ export default class Profile extends Component{
     }
     async componentDidMount(){
         console.log("test front",this.props.navigation.state.params.id)
-        await axios.get('https://192.168.3.142/api/getUser/'+this.props.navigation.state.params.id).then((response)=>{
+        await axios.get('https://192.168.0.172/api/getUser/'+this.props.navigation.state.params.id).then((response)=>{
             
             this.setState({
                 name: response.data.response[0].user_name,
